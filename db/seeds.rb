5782b2caef9cd59fe068efdf57e5730f6cfa6cc2
feliciebernard <felicie.bernard@outlook.fr>
2021-05-07 01:15:27 +0200

    ajout du seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+City.destroy_all
+Gossip.destroy_all
+Tag.destroy_all
+PrivateMessage.destroy_all
+
+10.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::TvShows::SiliconValley.email,
+    age: rand(18...100),
+    description: Faker::Food.description
+    )
+end
+
+10.times do
+  city = City.create(
+    name: Faker::Address.city, #
+    zip_code: Faker::Address.zip_code #
+    )
+end
+
+20.times do
+  gossip = Gossip.create(
+    title: Faker::DcComics.title,
+    content: Faker::ChuckNorris.fact
+    )
+end
+
+10.times do
+  tag = Tag.create( 
+    title: Faker::Game.genre
+    )
+end
+
+10.times do
+  privatemessage = PrivateMessage.create( 
+    content: Faker::GreekPhilosophers.quote
+    )
+end
